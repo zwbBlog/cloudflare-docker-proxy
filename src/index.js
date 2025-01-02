b7ae19ca7b8e9ad10405ae10600a5819061512a3
zwbBlog <34327410+zwbBlog@users.noreply.github.com>
2025-01-02 10:41:37 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import DOCS from './help.html'
+ 
+// return docs
+if (url.pathname === "/") {
+  return new Response(DOCS, {
+    status: 200,
+    headers: {
+      "content-type": "text/html"
+    }
+  });
+}
+
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
